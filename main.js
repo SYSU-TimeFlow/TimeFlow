7017edd542d04b3bcc0b696a1ae5483ecc269688
liaohr9 <liaohr9@mail2.sysu.edu.cn>
2025-05-21 22:48:02 +0800

    <feat> <calendar> Calendar ui 初始化
    完整的日历视图初始化。初始化的时候为了避免不同树节点中的通信，全部放在一个文件中，能够较好的专注于ui版面的设计和逻辑书写。

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-// import { app, BrowserWindow } from "electron";
-// import WinState from "electron-win-state";
-const { app, BrowserWindow } = require("electron"); // Electron 的主进程
-const WinState = require("electron-win-state").default; // 保持窗口状态的库
-const path = require("path"); // Node.js 的路径模块
+/**
+ * @description: Electron 主进程, 负责创建窗口和加载页面,一般不需要修改
+ */
+import { app, BrowserWindow } from "electron/main";
+import path from "path";
+import { fileURLToPath } from "url";
+import isDev from "electron-is-dev";
 
-// 主窗口（包含位置保持和大小保持）
+// 获取当前文件的路径
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// 创建窗口函数
 const createWindow = () => {
-  const win = WinState.createBrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: false,
-      // 预加载的js
-      preload: path.resolve(__dirname, "./preload/index.js"), // 预加载脚本
-    },
   });
 
-  //   win.loadFile("index.html");
-  win.loadURL("http://localhost:5173");
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
-  // 可能有错误提示，但是可以忽略的，是浏览器控制台的问题
-
-  console.log("Window created");
+  // 根据 模式 加载不同的页面
+  if (isDev) {
+    // 在开发模式下加载本地服务器
+    win.loadURL("http://localhost:5173");
+  } else {
+    // 在生产模式下加载vite打包后的文件
+    win.loadFile(path.join(__dirname, "dist/index.html"));
+  }
 };
 
+// 当 Electron 完成初始化时创建窗口（防止白屏等待加载初始化）
 app.whenReady().then(() => {
   createWindow();
 
-    app.on("activate", () => {
-        // 在 macOS 上，单击 Dock 图标时重新创建窗口
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
+// 监听窗口关闭事件
 app.on("window-all-closed", () => {
-  // 在 Windows 和 Linux 上，关闭所有窗口时退出应用
-  if (process.platform !== "darwin") app.quit();
-
-  console.log("All windows closed");
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
