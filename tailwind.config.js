a2fc3dffef037d890541f9bfc9f7fe5d2528db38
liudk <2735375422@qq.com>
2025-06-06 22:37:25 +0800

    refactor/重新删除没有必要添加的部分代码

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
-  safelist: [
-    'dark:text-[var(--heading-color)]',
-    'dark:bg-[var(--search-bg)]',
-    'dark:border-[var(--search-border)]',
-    'dark:text-[var(--text-primary)]',
-    'dark:text-[var(--text-tertiary)]',
-  ],
   theme: {
     extend: {
       // 你的自定义主题
