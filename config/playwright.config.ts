5cbb01c7cfcf966113222f1fa7e9e60ca0c29230
lizhx237 <lee3293276870@outlook.com>
2025-07-09 16:28:31 +0800

    fix(test): 修复playwright测试导入路径

--- config/playwright.config.ts
+++ config/playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     url: 'http://localhost:5173', // Vite 开发服务器的 URL
     reuseExistingServer: !process.env.CI, // 在 CI 环境中不重用现有服务器，本地可以重用
     timeout: 10 * 1000, // 等待服务器启动的超时时间 (例如 10 秒)
+    cwd: '../', // 指定在项目根目录下运行 npm run dev
   },
   projects: [
     {
@@ -14,7 +15,7 @@ export default defineConfig({
         // 启动 Electron 应用
         launchOptions: {
           executablePath: '../node_modules/.bin/electron', // Electron 可执行文件路径
-          args: ['.'], // 启动 Electron 时传入的参数，指向应用目录
+          args: ['./src/main/main.js'], // 显式指定 Electron 主进程入口
           headless: true, // 设置为 true 会无头模式运行
         },
       },
